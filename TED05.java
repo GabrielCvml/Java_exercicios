e1f5e5877c7c273676cdbcd53f7161f5b8f7ebf3
Gabriel Cavalcante <103397042+GabrielCvml@users.noreply.github.com>
2022-09-16 15:48:17 -0300

    Create TED05.java

---
+++ TED05.java
@@ -0,0 +1,19 @@
+public class TED05 {
+  
+    public static void main(String args[]) {
+      int cont = 1;
+      while (cont <= 20){
+          
+          if (cont%5 == 0){
+            System.out.print(cont + "-");
+            System.out.println('\t');
+          }
+          else{
+            System.out.print(cont + "-");
+            
+          }
+         cont++;
+      }
+      
+    }
+}
